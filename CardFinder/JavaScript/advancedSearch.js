f25f4974e17b7eef71e9148c0773e2d38382703b
Cweber25 <Onesockweber@gmail.com>
2023-02-24 14:52:47 -0500

    The side cards now show when sorting through a list

--- CardFinder/JavaScript/advancedSearch.js
+++ CardFinder/JavaScript/advancedSearch.js
@@ -3,12 +3,12 @@ var count = 0;
 var cardSpot = 0;
 
 cardList = async (e) => {
-    // Calls for one mana white cards
+  // Calls for one mana white cards
   const response = await fetch(
     `https://api.scryfall.com/cards/search?q=c%3Awhite+mv%3D1`
   );
   const card = await response.json();
-    // Puts them all in custom array
+  // Puts them all in custom array
   while (card.has_more == true && count <= 175) {
     cardL[count] = card.data[count];
     console.log(card.data[count]);
@@ -21,13 +21,128 @@ cardCallRight = async (e) => {
   const card = cardL[cardSpot];
   const imageEl = document.getElementById("card");
   imageEl.src = card.image_uris.normal;
-  cardSpot++;
+  moveRight();
+  //   cardSpot++;
+  applyInfoAndImage(card);
 };
 
 // Moves card left
 cardCallLeft = async (e) => {
-    const card = cardL[cardSpot];
-    const imageEl = document.getElementById("card");
-    imageEl.src = card.image_uris.normal;
-    cardSpot--;
-  };
+  const card = cardL[cardSpot - 1];
+  const imageEl = document.getElementById("card");
+  imageEl.src = card.image_uris.normal;
+  // cardSpot--;
+  moveLeft();
+  applyInfoAndImage(card);
+};
+
+// New idea create two seperate ones if left click it goes to new one but sends past vari to left
+moveRight = async (e) => {
+  cardSpot++;
+
+  try {
+    const cardLeft = cardL[cardSpot - 2];
+    const imageEl = document.getElementById("cardL");
+    imageEl.src = cardLeft.image_uris.normal;
+  } catch {
+    document.getElementById("cardL").src = "../images/mtgCardBack.jpg";
+  }
+
+  try {
+    const cardRight = cardL[cardSpot];
+    const imageEl2 = document.getElementById("cardR");
+    imageEl2.src = cardRight.image_uris.normal;
+
+    console.log(cardSpot);
+  } catch {
+    document.getElementById("cardR").src = "../images/mtgCardBack.jpg";
+  }
+};
+
+moveLeft = async (e) => {
+  cardSpot--;
+  try {
+    const cardLeft = cardL[cardSpot - 1];
+    const imageEl = document.getElementById("cardL");
+    imageEl.src = cardLeft.image_uris.normal;
+  } catch {
+    document.getElementById("cardL").src = "../images/mtgCardBack.jpg";
+  }
+
+  const cardRight = cardL[cardSpot + 1];
+  const imageEl2 = document.getElementById("cardR");
+  imageEl2.src = cardRight.image_uris.normal;
+
+  console.log(cardSpot);
+};
+
+function applyInfoAndImage(card) {
+  // Updating the card with the image data.
+  let imageEl = document.getElementById("card");
+  imageEl.src = card.image_uris.normal;
+
+  // Updating the Card Information Box with attributes from 'card' object.
+  let nameEl = document.getElementById("cardName");
+  nameEl.textContent = card.name;
+
+  let rarityEl = document.getElementById("cardRarity");
+  //let raritySymbol = document.getElementById("raritySymbol");
+
+  rarityImage = document.getElementById("raritySymbol");
+
+  if (card.rarity == "common") {
+    rarityEl.textContent = card.rarity;
+    rarityEl.style.color = "black";
+    rarityImage.src = "../images/commonSymbol.png";
+  }
+  if (card.rarity == "uncommon") {
+    rarityEl.textContent = card.rarity;
+    rarityEl.style.color = "#A0C3D3";
+    rarityImage.src = "../images/uncommonSymbol.png";
+  }
+  if (card.rarity == "rare") {
+    rarityEl.textContent = card.rarity;
+    rarityEl.style.color = "#D1B572";
+    rarityImage.src = "../images/rareSymbol.png";
+  }
+  if (card.rarity == "mythic") {
+    rarityEl.textContent = card.rarity;
+    rarityEl.style.color = "#E37704";
+    rarityImage.src = "../images/mythicSymbol.png";
+  }
+
+  let setEl = document.getElementById("cardSet");
+  setEl.textContent = card.set.toUpperCase();
+
+  let flavorTextEl = document.getElementById("cardFlavorText");
+  if (card.flavor_text != "") {
+    flavorTextEl.textContent = card.flavor_text;
+    flavorTextEl.style.visibility = "visible";
+  } else {
+    flavorTextEl.style.border = "none";
+  }
+
+  getPrice(card);
+}
+
+function getPrice(card) {
+  // Link Elements
+  let linkTCG = document.getElementById("linkTCG");
+  let linkCH = document.getElementById("linkCH");
+  // Price Elements
+  let usdPrice = document.getElementById("USDprice");
+  let usdFoilPrice = document.getElementById("USDfoilPrice");
+  if (card.prices.usd != null) {
+    usdPrice.textContent = "USD: $" + card.prices.usd;
+  } else {
+    usdPrice.textContent = "No USD Price Availbe";
+  }
+  if (card.prices.usd_foil != null) {
+    usdFoilPrice.textContent = "USD Foil: $" + card.prices.usd_foil;
+  } else {
+    usdFoilPrice.textContent = "No USD Foil Price Available";
+  }
+
+  linkTCG.href = card.purchase_uris.tcgplayer;
+  linkCH.href = card.purchase_uris.cardhoarder;
+}
